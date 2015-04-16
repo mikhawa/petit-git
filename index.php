13672d5b24f381089c1ce81a82274cc03bd29d35
michaelp <michael.pitz@cf2m.be>
2015-04-16 10:33:42 +0200

    Création de monjs.js et de la fonction bidon() Modification d'index.php pour intégrer monjs.js Création d'un bouton appellant cette fonction dans l'index

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <title>Projet petit-git</title>
+        <script src="monjs.js"></script>
     </head>
     <body>
         <h1>Projet petit-git</h1>
+        <button onclick="bidon();" value="Bidon !!!">Bidon!!!</button>
     </body>
 </html>
